b9550486398d78c93f76c7641beaa0c4a32312a4
Matthias Günther <matthias.guenther@wikimatze.de>
2013-05-25 14:47:27 +0200

    Add parsing logic for code blocks

---
+++ parsing.md
@@ -0,0 +1,14 @@
+aa
+
+
+aa
+
+
+{: lang="bash" }
+    $ mkdir ~/padrino-projects
+
+    $ cd ~/padrino_projects
+    $ padrino g project job-vacancy -d activerecord -t rspec -s jquery -e erb -a sqlite
+
+
+aa
---
+++ parsing.rb
@@ -0,0 +1,23 @@
+require 'pry'
+file = File.open("parsing.md")
+
+text = ''
+indicator = false
+# indicator_one = false
+# indicator_two = false
+
+body = file.readlines
+
+body.each_with_index do |line, number|
+  if line =~ /{(:.*=)(".*").*/
+    text << "```#{$2.gsub("\"", '')}```\n"
+    indicator = true
+  elsif indicator && body[number] == "\n" && body[number+1] == "\n"
+    text << "```\n\n"
+    indicator = false
+  else
+    text << line
+  end
+end
+
+puts text
