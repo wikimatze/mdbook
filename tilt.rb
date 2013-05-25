a79ef1f326be80fcb930044dd650a37c92febfce
Matthias Günther <matthias.guenther@wikimatze.de>
2013-05-25 08:47:13 +0200

    Removed header featurefor toc, will use jquery

--- tilt.rb
+++ tilt.rb
@@ -22,27 +22,6 @@ class HTMLwithPygments < Redcarpet::Render::HTML
     full_document
   end
 
-  def header(title, level)
-    @headers ||= []
-    # you can use this permalink style: 1-foo-bar with the level in it
-    permalink = "#{level}-#{title.gsub(/\W+/, "-")}"
-
-    # .. or just use title. you might like a better regex here, too
-#     permalink = title.gsub(/\W+/, "-")
-
-    # for extra credit: implement this as its own method
-    if @headers.include?(permalink)
-      permalink += "_1"
-      # my brain hurts
-      loop do
-        break if !@headers.include?(permalink)
-        # generate titles like foo-bar_1, foo-bar_2
-        permalink.gsub!(/\_(\d+)$/, "_#{$1.to_i + 1}")
-      end
-    end
-    @headers << permalink
-    %(\n<a name="#{permalink}" class="anchor" href="##{permalink}"><span class="anchor-icon">#{title}</span></a><h#{level} id=\"#{permalink}\">#{title}</h#{level}>\n)
-  end
 end
 
 def generate_output_path(path)
@@ -61,10 +40,10 @@ CHAPTERS.each do |chapter|
   chapter_file = File.open(chapter, 'r')
   chapter_path = chapter_file.path
 
-#   tmp = chapter_file
+  tmp = chapter_file
 #
-#    html_toc = Redcarpet::Markdown.new(Redcarpet::Render::HTML_TOC).render(tmp.read)
-#    puts html_toc
+    html_toc = Redcarpet::Markdown.new(Redcarpet::Render::HTML_TOC).render(tmp.read)
+    puts html_toc
 #
 #    puts html_toc
   # Inspiration: http://net.tutsplus.com/tutorials/ruby/ruby-for-newbies-the-tilt-gem/
